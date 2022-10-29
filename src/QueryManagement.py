fe90f1bb1bb19eb6085c7ae30cdc96f12bb82c23
Jermjerm69 <elitegamer1604@gmail.com>
2022-10-28 21:19:48 -0400

    Created Contains Query

--- src/QueryManagement.py
+++ src/QueryManagement.py
@@ -32,3 +32,19 @@ class QueryManager:
         if not (movie_found and rating_found):
             print(f"processing: LOOKUP {tconst}\nMovie not found!\nRating not found!")
         print(f"elapsed time:{elapsed}")
+
+    def contains(self,title_type,words):
+        """: Find all movies of a certain type whose titles contain the sequence of words.
+         :param filename: title_type,words
+         prints movies results
+         """
+        start = timer()
+        movie_found = False
+        for movie in self.movies:
+            if movie.titleType==title_type and words in movie.primaryTitle:
+                print(f"processing: CONTAINS {title_type} {words}")
+                print(f"Identifier: {movie.tconst}, Title: {movie.primaryTitle}, Type: {movie.titleType}, Year: {movie.startYear},Runtime: {movie.runtimeMinutes}, Genres: {movie.genres}")
+        if not movie_found:
+            print(f"processing: CONTAINS {title_type} {words}\nNo match found!")
+        elapsed = timer() - start
+        print(f"elapsed time:{elapsed}")
